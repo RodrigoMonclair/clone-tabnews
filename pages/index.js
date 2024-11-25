c32f997772701b41319d61bc1da7158e10b2d1e3
RodrigoMonclair <113136338+RodrigoMonclair@users.noreply.github.com>
2024-11-25 01:47:02 +0000

    modifica a frase

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,9 @@
 function Home(){
-    return <h1>Camila, eu amo você! Se você me ama, Sorria!</h1>
+    return <h1>Camila, eu amo você! Se você me ama, Sorria! 😎
+        Eu sou apaixonado por você, acredita nisso! Sei que estamos passando talvez por um momento
+        de desafios e novas empreitadas, mas vamos nos manter perseverantes e esperançosos em Deus!
+        Te amo!
+    </h1>
 }
 
 export default Home
