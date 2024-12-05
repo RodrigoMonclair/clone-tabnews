592120abc5d17085af4dab7691b4087eadaad74f
RodrigoMonclair <113136338+RodrigoMonclair@users.noreply.github.com>
2024-12-05 01:53:46 +0000

    add `database.js` and `.env` files

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -1,5 +1,8 @@
-function status(request, response) {
-  response.setHeader("Content-type", "text/plain; charset=utf-8");
+import database from "../../../../infra/database.js";
+
+async function status(request, response) {
+  const result = await database.query("SELECT 1 + 1 as sum;");
+  console.log(result.rows);
   response
     .status(200)
     .json({ chave: "os alunos do curso.dev são acima da média" });
